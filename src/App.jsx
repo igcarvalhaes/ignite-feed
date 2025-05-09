54f0e356a4ecd7825e36b5780509904b4cd71dc1
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-05-09 14:41:39 -0300

    Feat: add pasta components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import { Post } from "./Post";
-
+import { Header } from "./components/Header";
 export function App() {
   return (
     <>
-      <Post
-        author="Igor Carvalhaes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sunt explicabo voluptas velit dolorum animi illo. Consequatur eaque voluptatibus ut?"
-      />
-      <Post
-        author="Gabriel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sunt explicabo voluptas velit dolorum animi illo. Consequatur eaque voluptatibus ut?"
-      />
+      <Header />
     </>
   );
 }
