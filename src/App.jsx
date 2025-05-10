c9c44a487bc0dbd2d6ae86a1036c014b5b8c2f53
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-05-09 21:36:23 -0300

    Componente sidebar estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post
+            author="Igor Carvalhaes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptas animi quaerat distinctio culpa, deserunt laudantium nam facere nemo placeat?"
+          />
+          <Post
+            author="Carol Vianez"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptas animi quaerat distinctio culpa, deserunt laudantium nam facere nemo placeat?"
+          />
+        </main>
+      </div>
     </>
   );
 }
