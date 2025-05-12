938cd9ef0138e5745e49c06e3b5b06c5fb2122d7
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-05-12 14:56:54 -0300

    Comment: parte estrutural e estilizacao pronta

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -35,6 +36,22 @@ export function Post() {
             <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
             <a href="#">#rocketseat</a>
           </p>
+        </div>
+
+        <form className={styles.commentForm}>
+          <strong>Deixe seu feedeback</strong>
+
+          <textarea placeholder="Deixe um comentário" />
+
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
+
+        <div className={styles.commentList}>
+          <Comment />
+          <Comment />
+          <Comment />
         </div>
       </article>
     </>
