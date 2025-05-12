3a12abeaefd29b5d9e5dfa30005880b1bfb713d9
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-05-12 00:28:58 -0300

    Feat: Sidebar e Post

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "@phosphor-icons/react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -10,12 +12,19 @@ export function Sidebar() {
         />
 
         <div className={styles.profile}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/igcarvalhaes.png"
+          />
           <strong>Igor Carvalhaes</strong>
           <span>Web Developer</span>
         </div>
 
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
         </footer>
       </aside>
     </>
