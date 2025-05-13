679f78fdd082284b2d3b1b2e1fef7cecf57e4fb6
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-05-13 13:19:42 -0300

    Iteracao no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,51 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
+
 import "./global.css";
+
+// author: { avatar_url: "", namme: "", role: ""}
+// publishedAt: Date
+// content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/igcarvalhaes.png",
+      name: "Igor Carvalhaes",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-05-13 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/janedoe.png",
+      name: "Carol Vianez",
+      role: "UI/UX Designer",
+    },
+    content: [
+      { type: "paragraph", content: "Oi pessoal! 😊" },
+      {
+        type: "paragraph",
+        content:
+          "Compartilhando aqui meu mais novo projeto de design. Trabalhei bastante nos detalhes para criar uma interface incrível! O projeto se chama DesignHub 🎨",
+      },
+      { type: "link", content: "designhub.io/project" },
+    ],
+    publishedAt: new Date("2025-05-10 10:30:00"),
+  },
+];
 
 export function App() {
   return (
@@ -15,14 +59,15 @@ export function App() {
           <Sidebar />
         </aside>
         <main>
-          <Post
-            author="Igor Carvalhaes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptas animi quaerat distinctio culpa, deserunt laudantium nam facere nemo placeat?"
-          />
-          <Post
-            author="Carol Vianez"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptas animi quaerat distinctio culpa, deserunt laudantium nam facere nemo placeat?"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
