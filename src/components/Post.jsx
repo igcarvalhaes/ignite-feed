64fb32a9a338fff8dbadff1abda8f4c97525941b
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-05-14 20:21:40 -0300

    Feat: useState nos comentarios de post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,11 @@ import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
+import { useState } from "react";
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2]);
+
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -19,6 +22,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+  }
 
   return (
     <>
@@ -54,7 +63,7 @@ export function Post({ author, publishedAt, content }) {
           })}
         </div>
 
-        <form className={styles.commentForm}>
+        <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
           <strong>Deixe seu feedeback</strong>
 
           <textarea placeholder="Deixe um comentário" />
@@ -65,9 +74,9 @@ export function Post({ author, publishedAt, content }) {
         </form>
 
         <div className={styles.commentList}>
-          <Comment />
-          <Comment />
-          <Comment />
+          {comments.map((comment) => {
+            return <Comment />;
+          })}
         </div>
       </article>
     </>
