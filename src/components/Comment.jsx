41ce5e4e054029f529424554e1cbf903d4879c24
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-05-21 17:02:35 -0300

    Feat: Removendo comentarios

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,11 @@ import styles from "./Comment.module.css";
 
 import { Avatar } from "./Avatar.jsx";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <>
       <div className={styles.comment}>
@@ -21,7 +25,7 @@ export function Comment({ content }) {
                 </time>
               </div>
 
-              <button title="Deletar comentário">
+              <button onClick={handleDeleteComment} title="Deletar comentário">
                 <Trash size={24} />
               </button>
             </header>
